7f51bd6feaae16776febd6fb2ea70d8b4e5a9eb5
juune622 <juune622@naver.com>
2021-01-26 17:29:54 +0900

    ...01/26

--- routes/board-route.js
+++ routes/board-route.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const pugs = {
+	css: 'board', 
+	js:'board', 
+	title:'Express Board', 
+	headerTitle:'Node/Express를 활용한 게시판'
+}
 
 
+router.get('/',(req,res,next)=>{
+	res.render('board/list', {...pugs});
+});
+
+router.get('/create',(req,res,next)=>{
+	res.render('board/create', pugs);
+});
 
 
 module.exports = router;
